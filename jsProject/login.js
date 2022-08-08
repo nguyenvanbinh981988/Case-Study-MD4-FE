a11f9b128161fc306505a35a67a583f3a1359979
duong2512 <tungduong25122002@gmail.com>
2022-08-08 09:03:11 +0700

    ok

--- jsProject/login.js
+++ jsProject/login.js
@@ -30,7 +30,46 @@ function login() {
         }
     })
 }
+let listuser=null;
+$.ajax({
+    type: "GET",
+    headers: {
+        //kiểu dữ liệu nhận về
+        // 'Accept': 'application/json',
+        // kiểu truyền đi
+        'Content-Type': 'application/json'
+    },
+    url: "http://localhost:8180/appuser",
+    //xử lý khi thành công
+    success: function (data) {
+        listuser=data;
+    },
+    error: function (err) {
+        console.log(err)
+    }
+})
 function register(){
+    let arrUser = document.getElementsByTagName("input")
+    console.log(arrUser)
+    let userName = arrUser[3].value
+    let cmt = arrUser[4].value
+    let phone = arrUser[5].value
+    let eMail = arrUser[6].value
+    let pass = arrUser[7].value
+    if (userName=="" || cmt=="" || phone=="" || eMail==""||pass==""){
+        alert("Nhập đầy đủ các trường")
+        return;
+    }
+    if (isNaN(cmt)){
+        alert("Nhập Cmt không có chữ")
+        return;
+    }
+    if (isNaN(phone)){
+        alert("Nhập đúng số điện thoại")
+        return;
+    }
+
+
     let username = document.getElementById("nameUser").value;
     let cccdUser =document.getElementById("cccdUser").value;
     let phoneUser = document.getElementById("phoneUser").value;
@@ -43,20 +82,50 @@ function register(){
         email:email,
         passwordUser:passwordUser
     }
-    $.ajax({
-        type: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        url: "http://localhost:8080/register",
-        data: JSON.stringify(appUser),
-        //xử lý khi thành công
-        success: function (){
-            location.href = "Login.html"
-        },
-        error: function (err) {
-            console.log(err)
+    let check=true;
+    let messageUserName="";
+    let messagePhoneNumber="";
+    let messageEmail="";
+
+    for (let i = 0; i < listuser.length; i++) {
+        if (listuser[i].username==username){
+            messageUserName="Tài khoản đã tồn tại"
+            document.getElementById("messageUserName").innerText = messageUserName;
+            check=false;
         }
-    })
+        if (listuser[i].phoneUser==phoneUser){
+            messagePhoneNumber="Số điện thoại đã có người sử dụng";
+            document.getElementById("messagePhoneNumber").innerText = messagePhoneNumber;
+            check=false;
+        }
+        if (listuser[i].email==email){
+            messageEmail="Email đã có người sử dụng";
+            document.getElementById("messageEmail").innerText = messageEmail;
+            check=false;
+        }
+    }
+
+
+    if (check){
+        $.ajax({
+            type: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            url: "http://localhost:8080/register",
+            data: JSON.stringify(appUser),
+            //xử lý khi thành công
+            success: function (){
+                alert("đăng kí thành công")
+                location.href = "Login.html"
+                console.log("okoko")
+                console.log("okoko")
+                console.log("okoko")
+            },
+            error: function (err) {
+                console.log(err)
+            }
+        })
+    }
 }
